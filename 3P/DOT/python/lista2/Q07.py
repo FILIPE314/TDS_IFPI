b0447da4b96ba9d3fd6e2235ca68ae4b5b42b9be
Luiz Filipe de Carvalho <161400255+FILIPE314@users.noreply.github.com>
2025-04-06 13:26:59 -0300

    questão 7

---
+++ 3P/DOT/python/lista2/Q07.py
@@ -0,0 +1,19 @@
+# 7) Dada uma lista contendo 10 elementos numéricos, elabore um programa que verifique se um 
+# outro valor dado pertence ou não à lista.
+def verifica(lista, valor_aribuido):
+    for i in range(len(lista)):
+        if valor_aribuido == lista[i]:
+            return f'\nEsse valor já pertence a lista\n'
+    return f'\nEsse valor não pertence a essa lista\n'
+def main():
+    lista = list(range(50, 61))
+    while True:
+        try:
+            valor_aribuido = int(input('Digite um valor para saber se ele esta atribuido a uma lista: '))
+            print(verifica(lista, valor_aribuido))
+            print(lista)
+            break
+        except:
+            print(f'\nOps... Algo de errado não está certo, digite um valor válido!')
+if __name__ == '__main__':
+    main()
