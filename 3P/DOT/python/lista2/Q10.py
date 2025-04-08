501db75306c170225b7d2c594cd3b73f1fdf6875
Luiz Filipe de Carvalho <161400255+FILIPE314@users.noreply.github.com>
2025-04-07 23:10:03 -0300

    update questão 10

--- 3P/DOT/python/lista2/Q10.py
+++ 3P/DOT/python/lista2/Q10.py
@@ -1,6 +1,6 @@
 # 10) Faça um programa que grave uma lista com 15 posições, calcule e mostre: 
 # a) O maior elemento da lista e em que posição esse elemento se encontra; 
-# b)    O menor elemento da lista e em que posição esse elemento se encontra.
+# b) O menor elemento da lista e em que posição esse elemento se encontra.
 def maior_position(list):
     maior = list[0]
     position = -1
