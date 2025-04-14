c0f95cfec60a7e15e4b8e15c2a649e523edb2c73
Luiz Filipe de Carvalho <161400255+FILIPE314@users.noreply.github.com>
2025-04-14 02:30:02 -0300

    updates

--- 3P/DOT/Lista_1/questao_02.py
+++ 3P/DOT/Lista_1/questao_02.py
@@ -1,7 +1,6 @@
 # 2. Escreva um programa que leia o raio de um círculo e faça duas funções: uma função chamada área que calcula e retorna a área 
 # do círculo e outra função chamada perímetro que calcula e retorna o perímetro do círculo.
 # Área = PI * r2; Perímetro = PI * 2 * r;
-
 def area(raio):
     return 3.14 * raio **2
 
